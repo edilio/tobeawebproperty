d2d73331f843b5303e15852dfcd355ba8f8c87d8
Edilio Gallardo <edilio73@gmail.com>
2014-11-09 14:51:22 -0500

    customizing UserProfile Admin

--- haweb/apps/core/admin.py
+++ haweb/apps/core/admin.py
@@ -1,1 +1,20 @@
-__author__ = 'edilio'
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import UserProfile
+from django.utils.translation import ugettext_lazy as _
+
+
+class UserProfileAdmin(UserAdmin):
+    model = UserProfile
+
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+        (_('Photo'), {'fields': ('photo', )}),
+        (_('Title'), {'fields': ('title', )}),
+    )
+
+admin.site.register(UserProfile, UserProfileAdmin)
