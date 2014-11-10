a69a5d54ebc1ba68565e8fd6d03f51449f00106a
Edilio Gallardo <edilio73@gmail.com>
2014-11-09 20:37:03 -0500

    more detailed admin for Career, HelpfulLink and Faq

--- haweb/apps/web/admin.py
+++ haweb/apps/web/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from .models import FAQ, HelpfulLink
+from .models import FAQ, HelpfulLink, Career
 
-admin.site.register([FAQ, HelpfulLink])
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('index', 'question', 'short_answer')
+    search_fields = ('question', 'answer')
+
+
+@admin.register(HelpfulLink)
+class HelpfulLinkAdmin(admin.ModelAdmin):
+    list_display = ('index', 'title', 'url', 'short_description')
+    search_fields = ('title', 'description')
+
+
+@admin.register(Career)
+class CareerAdmin(admin.ModelAdmin):
+    list_display = ('index', 'title', 'short_description')
+    search_fields = ('title', 'description')
--- haweb/apps/web/models.py
+++ haweb/apps/web/models.py
@@ -4,6 +4,7 @@ from django.db.models.loading import get_model
 
 def next_index(model_name, app='web'):
     model = get_model(app, model_name)
+
     qs = model.objects
     try:
         m = qs.reverse()[0]
@@ -12,28 +13,61 @@ def next_index(model_name, app='web'):
         return 1
 
 
-def next_faq():
+def next_faq_index():
     return next_index('FAQ')
 
 
-def next_link():
-    return next_index('HelpfulLink')
-
-
 class FAQ(models.Model):
-    index = models.PositiveIntegerField(default=next_faq)
+    index = models.PositiveIntegerField(default=next_faq_index)
     question = models.CharField(max_length=250)
     answer = models.TextField()
 
     def __unicode__(self):
         return self.question[:25]
 
+    @property
+    def short_answer(self):
+        return self.answer[:250]
+
+
+def next_helpful_link_index():
+    return next_index('HelpfulLink')
+
 
 class HelpfulLink(models.Model):
-    index = models.PositiveIntegerField(default=next_link)
+    index = models.PositiveIntegerField(default=next_helpful_link_index)
     title = models.CharField(max_length=250)
     url = models.URLField()
     description = models.TextField()
+
+    def __unicode__(self):
+        return self.title
+
+    @property
+    def short_description(self):
+        return self.description[:250]
+
+
+def next_career_index():
+    return next_index('Career')
+
+
+class Career(models.Model):
+    index = models.PositiveIntegerField(default=next_career_index)
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+
+    def __unicode__(self):
+        return self.title
+
+    @property
+    def short_description(self):
+        return self.description[:250]
+
+
+class ResourceCategory(models.Model):
+    index = models.PositiveIntegerField(default=next_career_index)
+    name = models.CharField(max_length=250)
 
     def __unicode__(self):
         return self.title
