be1fa80dcc4ce3110992faf7dd209d7ba865a739
Edilio Gallardo <edilio73@gmail.com>
2014-11-17 23:30:55 -0500

    improvements to static files in prod and fab.py

--- fabfile.py
+++ fabfile.py
@@ -5,14 +5,14 @@ from fabric.operations import get
 pip install fabric
 
 how to run:
-fab deploy -i ~/.ssh/dak-edilio-only.pem -H ubuntu@www.crowdconcerns.com
+fab deploy -H root@www.jedutils.com
 """
 
 @task
 def deploy():
-    prob_home = '/var/django/crowdconcerns.com/concernsprj'
+    prob_home = '/var/www/django/tobeawebproperty'
     with cd(prob_home):
         sudo('git pull')
         sudo('./manage.py collectstatic --noinput')
         sudo('./manage.py migrate')
-        sudo('/etc/init.d/supervisor restart')
+        # sudo('/etc/init.d/supervisor restart')
