95d5d8e855485c308b2360d6980958a858af38d2
Edilio Gallardo <edilio73@gmail.com>
2014-12-22 12:55:29 -0500

    improving fabfile to new server

--- fabfile.py
+++ fabfile.py
@@ -8,12 +8,19 @@ how to run:
 fab deploy -H root@demo.jedutils.com
 """
 
+
+ENV_NAME = 'haweb'
+PORT = 8003
+
+
+def gen_unicorn_cmd():
+    return 'gunicorn -w 2 -b 127.0.0.1:{} -n {} haweb.wsgi:application'.format(PORT, ENV_NAME)
+
 @task
 def deploy():
     prob_home = '/var/www/django/tobeawebproperty'
     with cd(prob_home):
         sudo('git pull')
-        run('workon haweb && ./manage.py collectstatic --noinput')
-        run('workon haweb && ./manage.py migrate')
-        run('workon haweb && gunicorn -w 2 -p 8000 -n haweb haweb.wsgi:application &')
-        # sudo('/etc/init.d/supervisor restart')
+        run('workon {} && python manage.py collectstatic --noinput'.format(ENV_NAME))
+        run('workon {} && python manage.py migrate'.format(ENV_NAME))
+        run('workon {} && {} &'.format(ENV_NAME, gen_unicorn_cmd()))
